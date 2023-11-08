51e4ac7732971c5a53440b2614813f0beea33c1d
Bonifasius Martin <martinwibawa@gmail.com>
2023-11-08 18:11:12 +0700

    Added Breeze (includes react, inertia)

--- app/Models/Shipment.php
+++ app/Models/Shipment.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Shipment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['preorder_id', 'service_provider', 'driver', 'plat', 'no_hp', 'link', 'keterangan'];
+
+    protected $guarded = ['id'];
 }
