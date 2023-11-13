6f56eebc7842fdc7c1bcc2c8db0c9d6576d48723
nasbi24 <nasbi24@gmail.com>
2023-11-13 16:53:44 +0700

    animation & rapiin

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,21 +12,30 @@
 </head>
 
 <body>
-<div class="overflow-scroll flex flex-col items-center bg-[#FF6400] overflow-x-hidden relative">
-    <div class="h-screen">
+<div class="overflow-y-scroll flex flex-col items-center bg-[#FF6400] overflow-x-hidden relative ">
+    <!-- section - home -->
+    <div class="h-screen relative">
+
+        <img class="absolute h-96 z-10 -right-56 bottom-10 mt-6 animate-wiggle opacity-70" src="/images/awan.webp" alt="Logo">
+        <img class="absolute h-96 z-10 -left-60 -bottom-32 animate-wiggle opacity-70" src="/images/awan.webp" alt="Logo">
+        <div class="absolute h-80 z-10 left-20 top-32 animate-move-x">
+            <img class="animate-move-y opacity-70 h-80" src="/images/awan.webp" alt="Logo">
+        </div>
+
+
         <!-- navbar -->
-        <div id="navbar" class="flex justify-between w-screen h-16 pt-3 pr-5 items-center z-10">
-            <div class="w-[10%]flex items-center justify-center">
+        <div id="navbar" class="flex justify-between w-screen h-16 pt-3 pr-5 items-center z-10 mt-2">
+            <div class="w-[10%]flex items-center justify-center z-10">
                 <img class="object-cover h-16" src="/images/logo.webp" alt="Logo">
             </div>
-            <div class="w-[75%] flex h-full px-10 rounded-full border-2 border-yellow-800 bg-orange-300 items-center justify-between">
+            <div class="w-[75%] flex h-full px-10 rounded-full border-2 border-yellow-800 bg-orange-300 items-center justify-between z-10">
                 <a href="" class="text-orange-800 font-averialibre hover:font-averialibrebold text-2xl">Home</a>
                 <a href="" class="text-orange-800 font-averialibre hover:font-averialibrebold text-2xl">Bulk Order</a>
                 <a href="" class="text-orange-800 font-averialibre hover:font-averialibrebold text-2xl">Franchise</a>
                 <a href="" class="text-orange-800 font-averialibre hover:font-averialibrebold text-2xl">About Us</a>
                 <a href="" class="text-orange-800 font-averialibre hover:font-averialibrebold text-2xl">My Dashboard</a>
             </div>
-            <button href="" class="w-[12%] flex h-full ml-3 rounded-full border-2 border-yellow-800 bg-orange-300 items-center justify-center">
+            <button href="" class="w-[12%] hover:scale-[1.2] duration-75 flex h-full ml-3 rounded-full border-2 border-yellow-800 bg-orange-300 items-center justify-center">
                     <div class="w-8 flex items-center justify-center">
                         <img class="object-cover invert" src="/images/avatar.webp" alt="Logo">
                     </div>
@@ -57,20 +66,21 @@
                 </button>
             </div>
         </div>
+
     </div>
 
-    <div class="w-full relative">
+    <div class="w-full">
         <div class="absolute w-screen z-0 scale-[1.7] lg:-mt-56 md:-mt-48">
             <img class="object-cover animate-spin-slow" src="/images/cahaya.webp" alt="Logo">
         </div>
     </div>
 
-    <!-- menu -->
+    <!-- section - menu -->
     <div class="w-full flex flex-col items-center z-10" style="background-image: url('/images/background-menu.webp');">
-        <p class="text-orange-800 font-averialibre text-5xl mx-auto my-10" style="text-shadow: 4px 4px 4px #FFFFFF, -4px -4px 4px #FFFFFF, 4px -4px 4px #FFFFFF, -4px 4px 4px #FFFFFF;">Our Menu</p>
-        <div class="flex flex-row w-[90%] mt-10">
+        <p class="text-orange-800 font-averialibre text-5xl mx-auto my-5" style="text-shadow: 4px 4px 4px #FFFFFF, -4px -4px 4px #FFFFFF, 4px -4px 4px #FFFFFF, -4px 4px 4px #FFFFFF;">Our Menu</p>
+        <div class="flex flex-row w-[90%] mt-2">
             <div class="w-[50%]">
-                <div class="w-[80%] flex items-center justify-center mx-auto mb-16">
+                <div class="w-[80%] flex items-center justify-center mx-auto mb-4">
                     <img class="object-cover" src="/images/logo3.webp" alt="Logo">
                 </div>
                 <div class="">
@@ -79,110 +89,117 @@
             </div>
             <div class="w-[50%] flex flex-col">
                 <!-- SERIES #1 -->
-                <p class="text-orange-800 font-averialibre text-5xl mx-auto" style="text-shadow: 4px 4px 4px #FFFFFF, -4px -4px 4px #FFFFFF, 4px -4px 4px #FFFFFF, -4px 4px 4px #FFFFFF;">COKLAT SERIES</p>
-                <div class="grid grid-cols-2 w-[95%] mx-auto mt-2 mb-4">
+                <p class="text-orange-800 font-averialibre text-4xl mx-auto" style="text-shadow: 4px 4px 4px #FFFFFF, -4px -4px 4px #FFFFFF, 4px -4px 4px #FFFFFF, -4px 4px 4px #FFFFFF;">COKLAT SERIES</p>
+                <div class="grid grid-cols-2 w-[80%] mx-auto mt-3">
                     <!-- RASA -->
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Coklat</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Coklat</p>
                     </div>
 
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Coklat keju</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Coklat keju</p>
                     </div>
 
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Coklat vanilla</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Coklat vanilla</p>
                     </div>
 
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Coklat Oreo</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Coklat Oreo</p>
                     </div>
 
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Coklat caramel</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Coklat caramel</p>
                     </div>
                     
                 </div>
 
                 <!-- SERIES #2 -->
-                <p class="text-orange-800 font-averialibre text-5xl mx-auto" style="text-shadow: 4px 4px 4px #FFFFFF, -4px -4px 4px #FFFFFF, 4px -4px 4px #FFFFFF, -4px 4px 4px #FFFFFF;">VANILLA SERIES</p>
-                <div class="grid grid-cols-2 w-[95%] mx-auto mt-2 mb-4">
+                <p class="text-orange-800 font-averialibre text-4xl mx-auto" style="text-shadow: 4px 4px 4px #FFFFFF, -4px -4px 4px #FFFFFF, 4px -4px 4px #FFFFFF, -4px 4px 4px #FFFFFF;">VANILLA SERIES</p>
+                <div class="grid grid-cols-2 w-[80%] mx-auto mt-3">
                     <!-- RASA -->
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Vanilla</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Vanilla</p>
                     </div>
 
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Vanilla Keju</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Vanilla Keju</p>
                     </div>
 
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Vanilla Oreo</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Vanilla Oreo</p>
                     </div>
 
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Vanilla Caramel</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Vanilla Caramel</p>
                     </div>      
                 </div>
 
                 <!-- SERIES #3 -->
-                <p class="text-orange-800 font-averialibre text-5xl mx-auto" style="text-shadow: 4px 4px 4px #FFFFFF, -4px -4px 4px #FFFFFF, 4px -4px 4px #FFFFFF, -4px 4px 4px #FFFFFF;">OTHER SERIES</p>
-                <div class="grid grid-cols-2 w-[95%] mx-auto mt-2 mb-4">
+                <p class="text-orange-800 font-averialibre text-4xl mx-auto" style="text-shadow: 4px 4px 4px #FFFFFF, -4px -4px 4px #FFFFFF, 4px -4px 4px #FFFFFF, -4px 4px 4px #FFFFFF;">OTHER SERIES</p>
+                <div class="grid grid-cols-2 w-[80%] mx-auto mt-3">
                     <!-- RASA -->
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Strawberry</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Strawberry</p>
                     </div>
 
-                    <div class="flex mt-2">
+                    <div class="flex">
                         <div class="w-8 flex items-center justify-center mr-2">
                             <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
                         </div>
-                        <p class="text-orange-800 font-averialibre text-4xl">Blueberry</p>
+                        <p class="text-orange-800 font-averialibre text-3xl">Blueberry</p>
+                    </div>
+
+                    <div class="flex">
+                        <div class="w-8 flex items-center justify-center mr-2">
+                            <img class="object-cover h-14 transform scale-x-[-1] -translate-y-2" src="/images/ikan.webp" alt="Logo">
+                        </div>
+                        <p class="text-orange-800 font-averialibre text-3xl">Kacang merah</p>
                     </div>
 
                 </div>
             </div>
         </div>
-        <button href="" class="flex h-28 w-[50%] rounded-full border-2 border-yellow-800 bg-[#FF6400] items-center justify-center mx-4 my-16">
+        <button href="" class="flex h-28 w-[50%] rounded-full border-2 border-yellow-800 bg-[#FF6400] items-center justify-center mx-4 mt-12 mb-16">
             <p class="text-white font-averialibre text-5xl">PRE ORDER SEKARANG!</p>
         </button>
     </div>
 
-    <!-- franchise -->
+    <!-- section - franchise -->
     <div class="w-full flex flex-col relative items-center bg-[#F1A03C]">
 
-        <img class="absolute h-96 z-0 -left-64 mt-5" src="/images/awan.webp" alt="Logo">
-        <img class="absolute h-96 z-0 -right-64 bottom-0 mt-5" src="/images/awan.webp" alt="Logo">
+        <img class="absolute h-96 z-0 -left-64 mt-5 animate-wiggle" src="/images/awan.webp" alt="Logo">
+        <img class="absolute h-96 z-0 -right-64 bottom-0 mt-5 animate-wiggle" src="/images/awan.webp" alt="Logo">
 
         <div class="flex flex-row w-[80%] mt-10 z-10 ">
             <div class="w-[50%]">
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,27 @@ module.exports = {
   ],
   theme: {
     extend: {
+      
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-5deg)' },
+          '50%': { transform: 'rotate(5deg)' },
+        },
+        moveY: {
+          '0%, 100%': { transform: 'translateY(-25%)' },
+          '50%': { transform: 'translateY(0%)' },
+        },
+        moveX: {
+          '0%, 100%': { transform: 'translateX(20%)' },
+          '50%': { transform: 'translateX(0%)' },
+        }
+      },
       animation: {
-        'spin-slow': 'spin 5s linear infinite',
+        'move-x': 'moveX 14s ease-in-out infinite',
+        'move-y': 'moveY 10s ease-in-out infinite',
+        'wiggle': 'wiggle 6s ease-in-out infinite',
+        'spin-slow': 'spin 7s linear infinite',
+        'bounce-slow': 'bounce 3s infinite',
       },
       fontFamily: {
         averialibrebold: ['Averia Libre Bold'],
